cb16ac3d8f34da58ab4a9937fa8b71f64d38763c
Asadbek Eshboev <aeshboyev@texnomart.uz>
2022-09-21 15:04:50 +0500

    refactor: template craet

--- src/pages/index/datas/template.ts
+++ src/pages/index/datas/template.ts
@@ -3,7 +3,7 @@ import TemplateTypes, { Content } from "@/helpers/template-types.ts";
 interface TTypes {
   image: string;
   desc: string;
-  content: Content;
+  content: Partial<Content>;
 }
 
 const templates: TTypes[] = [
@@ -13,21 +13,6 @@ const templates: TTypes[] = [
     content: {
       widgetName: "raShowcase",
       type: "roll",
-      blocks: [
-        {
-          imgLink: "link",
-          img: {
-            src: "link",
-            srcMobile: "link",
-            alt: "Текстовое описание изображения",
-            width: 2832,
-            height: 1416,
-            widthMobile: 640,
-            heightMobile: 640,
-            position: "width_one_third",
-          },
-        },
-      ],
     },
   },
   {
@@ -36,35 +21,6 @@ const templates: TTypes[] = [
     content: {
       widgetName: "raShowcase",
       type: "billboard",
-      blocks: [
-        {
-          imgLink: "",
-          img: {
-            src: "https://cdn1.ozone.ru/s3/rich-content/placeholder/1416x708.png",
-            srcMobile:
-              "https://cdn1.ozone.ru/s3/rich-content/placeholder/640x640.png",
-            alt: "Текстовое описание изображения",
-            width: 1416,
-            height: 708,
-            widthMobile: 640,
-            heightMobile: 640,
-          },
-          title: {
-            content: ["Заголовок"],
-            size: "size4",
-            align: "left",
-            color: "color1",
-          },
-          text: {
-            size: "size2",
-            align: "left",
-            color: "color1",
-            content: [
-              "Пожалуйста, замените этот текст Вашим собственным. Просто кликните по тексту, чтобы добавить свой текст. Настройте стиль текста в левой колонке.",
-            ],
-          },
-        },
-      ],
     },
   },
   // {
