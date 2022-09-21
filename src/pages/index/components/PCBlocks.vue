221ad0a9548f560ba193ca6cc5b4bb674fb6f4a8
Asadbek Eshboev <aeshboyev@texnomart.uz>
2022-09-21 14:28:26 +0500

    refactor: drag and drop

--- src/pages/index/components/PCBlocks.vue
+++ src/pages/index/components/PCBlocks.vue
@@ -2,30 +2,50 @@
   <div class="bg-[#001a34] h-full text-white p-4">
     <h2 class="font-semibold text-[25px]">Блоки</h2>
     <div class="flex flex-col gap-8 mt-8">
-      <div
-        v-for="(item, index) in templates"
-        :key="index"
-        @click="handleClick(item.content)"
-        class="selectDisable"
+      <VueDraggableNext
+        class="dragArea list-group w-full"
+        :list="templates"
+        :group="{ name: 'people', pull: 'clone', put: false }"
+        :sort="false"
+        @change="log"
+        :move="checkMove"
       >
-        <img
-          draggable="true"
-          :src="item.image"
-          class="!max-w-full !w-full select-none"
-        />
-        <p class="mt-2 text-white text-[14px] leading-[18px]">
-          {{ item.desc }}
-        </p>
-      </div>
+        <div
+          v-for="(item, index) in templates"
+          :key="index"
+          class="selectDisable"
+        >
+          <img
+            draggable="true"
+            :src="item.image"
+            class="!max-w-full !w-full select-none"
+          />
+          <p class="mt-2 text-white text-[14px] leading-[18px]">
+            {{ item.desc }}
+          </p>
+        </div>
+      </VueDraggableNext>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { VueDraggableNext } from "vue-draggable-next";
+
 import templates from "../datas/template";
 
-function handleClick(item) {
-  console.log(item);
+function checkMove(event) {
+  console.log("checkMove", event.draggedContext);
+  console.log("Future index: " + event.draggedContext.futureIndex);
+}
+function log(event) {
+  const { moved, added } = event;
+  if (moved) {
+    console.log("moved", moved);
+  }
+  if (added) {
+    console.log("added", added, added.element);
+  }
 }
 </script>
 
--- src/pages/index/components/PCDropZone.vue
+++ src/pages/index/components/PCDropZone.vue
@@ -1,38 +1,51 @@
 <template>
-  <div>
-    <h2 class="font-bold text-[30px]">Конструктор рич-контента</h2>
-
-    <div
-      class="border-2 border-[#ccc] border-dotted mt-8 h-[700px] flex-center-center"
+  <div class="">
+    <VueDraggableNext
+      class="dragArea list-group w-full"
+      :list="templates"
+      group="people"
+      @change="log"
+      :move="checkMove"
     >
-      <p class="text-[20px] font-semibold">Drop Here</p>
-    </div>
+      <div class="flex flex-col gap-5" v-if="templates?.length">
+        <div v-for="(item, index) in templates" :key="index" class="">
+          <img class="w-full h-[708px] object-cover" :src="item.image" />
+        </div>
+      </div>
+
+      <div v-else>
+        <h2 class="font-bold text-[30px]">Конструктор рич-контента</h2>
+
+        <div
+          class="border-2 border-[#ccc] border-dotted mt-8 h-[700px] flex-center-center"
+        >
+          <p class="text-[20px] font-semibold">Drop Here</p>
+        </div>
+      </div>
+    </VueDraggableNext>
+    <pre>{{ templates }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
-const content = [
-  {
-    widgetName: "raShowcase",
-    type: "roll",
-    blocks: [
-      {
-        imgLink: "https://ozon.ru/",
-        img: {
-          src: "https://cdn1.ozone.ru/s3/rich-content/placeholder/1416x708.png",
-          srcMobile:
-            "https://cdn1.ozone.ru/s3/rich-content/placeholder/640x640.png",
-          alt: "ALT TEXT",
-          width: 1416,
-          height: 708,
-          widthMobile: 640,
-          heightMobile: 640,
-          position: "width_one_fourth",
-        },
-      },
-    ],
-  },
-];
+import { ref } from "vue";
+import { VueDraggableNext } from "vue-draggable-next";
+
+const templates = ref([]);
+
+function checkMove(event) {
+  console.log("checkMove", event.draggedContext);
+  console.log("Future index: " + event.draggedContext.futureIndex);
+}
+function log(event) {
+  const { moved, added } = event;
+  if (moved) {
+    console.log("moved", moved);
+  }
+  if (added) {
+    console.log("added", added, added.element);
+  }
+}
 </script>
 
 <style scoped></style>
