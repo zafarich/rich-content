05803002b900f9d3da8db0d6aed072886ef96c93
Asadbek Eshboev <aeshboyev@texnomart.uz>
2022-09-22 11:43:06 +0500

    refactor: style of content

--- src/components/CPreviewContent.vue
+++ src/components/CPreviewContent.vue
@@ -18,21 +18,35 @@
       <template v-if="content?.length">
         <transition-group type="transition" name="flip-list">
           <div
-            class="list-group-item flex bg-white border-grey border-[1px] rounded-[12px] p-6"
+            class="list-group-item flex gap-[10px] w-full"
             v-for="(item, index) in content"
             :key="index"
           >
-            <img
-              class="w-full h-[708px] object-cover select-none"
-              :src="item.content.block.img.src"
-            />
-            <div class="inline">
-              <Icon
-                @click="deleteContent(index)"
-                name="trash_bin"
-                color="#001a34"
-                class="w-8 h-8 hover:opacity-75 transition"
+            <div
+              class="bg-white border-grey border-[1px] rounded-[12px] p-6 preview__content"
+            >
+              <img
+                class="w-full h-[708px] object-cover select-none"
+                :src="item.content.block.img.src"
               />
+            </div>
+            <div class="w-8 flex flex-col gap-[18px] cursor-default">
+              <div class="bg-white-100 rounded w-8 h-8 flex-center-center">
+                <Icon
+                  @click="deleteContent(index)"
+                  name="trash_bin"
+                  color="#001a34"
+                  class="w-5 h-5 hover:opacity-75 transition"
+                />
+              </div>
+              <div class="bg-white-100 rounded w-8 h-8 flex-center-center">
+                <Icon
+                  @click="deleteContent(index)"
+                  name="arrow_down"
+                  color="#001a34"
+                  class="w-5 h-5 hover:opacity-75 transition"
+                />
+              </div>
             </div>
           </div>
         </transition-group>
@@ -96,5 +110,9 @@ const { content, deleteContent } = useContent();
 .preview__drop {
   background: linear-gradient(0deg, #ffffff, #ffffff), #f7f7f7;
   box-shadow: 0px 4px 24px 1px rgba(0, 0, 0, 0.08);
+}
+
+.preview__content {
+  width: calc(100% - 42px);
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
         grey: {
           DEFAULT: "#D5D8DF",
         },
+        white: {
+          DEFAULT: "#fff",
+          100: "#F5F7F9",
+        },
+      },
+
+      borderRadius: {
+        none: "0",
+        DEFAULT: "8px",
+        10: "10px",
+        12: "12px",
       },
     },
   },
