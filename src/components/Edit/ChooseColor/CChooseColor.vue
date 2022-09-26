c8c4140ddac39fa962368848ccf998948de32994
Асадбек Эшбоев Шавкат угли <aeshboyev@office.tm.uz>
2022-09-26 17:53:16 +0500

    refactor: border in choose color

--- src/components/Edit/ChooseColor/CChooseColor.vue
+++ src/components/Edit/ChooseColor/CChooseColor.vue
@@ -3,11 +3,13 @@
     <h4 class="font-medium text-[14px] leading-[20px] mb-2">Цвет текста</h4>
     <div class="flex-center gap-2">
       <div
-        class="w-8 h-8 border-[1px] border-solid border-grey rounded outline-[1px] outline-transparent outline cursor-pointer"
+        class="w-8 h-8 border-[1px] border-solid border-grey rounded outline-[1px] outline-transparent outline cursor-pointer transition"
         v-for="(item, index) in colors"
         :key="index"
         :style="`background-color: ${item}`"
-        :class="{ 'border-yellow outline-yellow': activeColor == item }"
+        :class="{
+          '!border-yellow !outline-yellow transition': activeColor == item,
+        }"
         @click="handleColor(item)"
       ></div>
     </div>
