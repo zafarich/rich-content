5f041bf6270834eb575ccdc58a218badb3c789fa
Asadbek Eshboev <asadullakkh@mail.ru>
2022-10-12 10:09:36 +0500

    refactor: todos

--- src/pages/PIndex.vue
+++ src/pages/PIndex.vue
@@ -55,15 +55,15 @@ function showAlertBeforeMount(event: object) {
 // remove id from LS in second times
 
 // TODO:
-// 1. Preview
+// 1. Preview - Jasur
 // 2. image & video id
 // 3. storybook
 // 4. backend
 // 5. texnomart front
 // 6. edit
 // 7. active indexx
-// 8. table controller
-// refactor
+// 8. table controller - Asad
+// refactor depends on mood :)
 </script>
 
 <style scoped>
