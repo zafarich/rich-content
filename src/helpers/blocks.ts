d9ed9d9d5056f2813215e928335b950f0e91cd4f
norbekoff <jasurbeknorbekov@gmail.com>
2022-10-18 13:33:54 +0500

    refactor: images

--- src/helpers/blocks.ts
+++ src/helpers/blocks.ts
@@ -10,52 +10,52 @@ interface TTypes {
 
 const blocks: TTypes[] = [
   {
-    block_img: "/src/assets/images/blocks/block1.png",
+    block_img: "/images/blocks/block1.png",
     block_text: "Изображение без отступов",
     content: Scheme["roll"],
   },
   {
-    block_img: "/src/assets/images/blocks/block2.png",
+    block_img: "/images/blocks/block2.png",
     block_text: "Изображение с текстом",
     content: Scheme["billboard"],
   },
   {
-    block_img: "/src/assets/images/blocks/block3.png",
+    block_img: "/images/blocks/block3.png",
     block_text: "Изображение и текст слева",
     content: Scheme["chess"],
   },
   {
-    block_img: "/src/assets/images/blocks/block4.png",
+    block_img: "/images/blocks/block4.png",
     block_text: "Изображение и текст справа",
     content: Scheme["chessReverse"],
   },
   {
-    block_img: "/src/assets/images/blocks/block5.png",
+    block_img: "/images/blocks/block5.png",
     block_text: "Две карточки с фото",
     content: Scheme["twoRow"],
   },
   {
-    block_img: "/src/assets/images/blocks/block6.png",
+    block_img: "/images/blocks/block6.png",
     block_text: "Две карточки с фото",
     content: Scheme["threeRow"],
   },
   {
-    block_img: "/src/assets/images/blocks/block7.png",
+    block_img: "/images/blocks/block7.png",
     block_text: "Видео",
     content: Scheme["video"],
   },
   {
-    block_img: "/src/assets/images/blocks/block8.png",
+    block_img: "/images/blocks/block8.png",
     block_text: "Текст",
     content: Scheme["text"],
   },
   {
-    block_img: "/src/assets/images/blocks/block9.png",
+    block_img: "/images/blocks/block9.png",
     block_text: "Список",
     content: Scheme["list"],
   },
   {
-    block_img: "/src/assets/images/blocks/block10.png",
+    block_img: "/images/blocks/block10.png",
     block_text: "Таблица",
     content: Scheme["table"],
   },
