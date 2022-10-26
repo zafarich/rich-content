889ae13521167cce23f41a9740df493d54f04369
Asadbek Eshboev <asadullakkh@mail.ru>
2022-10-26 13:31:28 +0500

    refactor: preloader

---
+++ src/components/Preloader/CPreloader.stories.ts
@@ -0,0 +1,27 @@
+import { Story } from "@storybook/vue3"
+
+import CPreloader, { Props } from "./CPreloader.vue"
+
+export default {
+  title: "Trash/Form/CPreloader",
+  component: CPreloader,
+}
+
+const Template: Story<Props> = (args) => ({
+  components: { CPreloader },
+  setup() {
+    return { args }
+  },
+  template: `
+    <div>
+      <block-preloader v-bind="args">
+        <div class="w-[200px] height-[12px]">HELLO!!!!</div>
+      </block-preloader>
+    </div>
+  `,
+})
+
+export const KPreloader = Template.bind({})
+KPreloader.args = {
+  loading: true,
+}
---
+++ src/components/Preloader/CPreloader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="overflow-hidden relative">
+    <div
+      v-if="loading"
+      :class="preloaderClass"
+      class="v-skeleton-loader__image v-skeleton-loader__bone  !w-full">
+    </div>
+    <template v-else>
+      <slot />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface Props {
+  loading?: boolean
+  preloaderClass?: string | Array<any>
+}
+const props = withDefaults(defineProps<Props>(), {
+  preloaderClass: "",
+})
+</script>
+
+<style scoped>
+.v-skeleton-loader__image {
+    position: relative;
+    background: rgba(0,0,0,.12);
+}
+
+.v-skeleton-loader__bone:after {
+    -webkit-animation: loading 1.5s infinite;
+    animation: loading 1.5s infinite;
+    content: "";
+    height: 100%;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transform: translateX(-100%);
+    z-index: 1;
+}
+
+.v-skeleton-loader__bone:after {
+    background: linear-gradient(90deg,hsla(0,0%,100%,0),hsla(0,0%,100%,.3),hsla(0,0%,100%,0));
+}
+
+@-webkit-keyframes loading {
+    to {
+        transform: translateX(100%)
+    }
+}
+
+@keyframes loading {
+    to {
+        transform: translateX(100%)
+    }
+}
+</style>
